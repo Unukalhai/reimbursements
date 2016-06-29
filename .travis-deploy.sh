d576d2de69b159cfa691be1a42fc0a8234bcf6f0
greg2010 <greg2008200@gmail.com>
2016-06-29 15:04:36 -0400

    yourself

---
+++ .travis-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ssh-keyscan -t rsa,dsa,ecdsa -H $travis_deploy_hostname >> $HOME/.ssh/known_hosts
+sftp $travis_deploy_username@$travis_deploy_hostname << !
+  mkdir $TRAVIS_BUILD_ID
+  lcd $TRAVIS_BUILD_DIR/build
+  cd $TRAVIS_BUILD_ID
+  put * -r
+  bye
+!
